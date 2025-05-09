8cb152c321995a01f810a6065daddd565e6d9ac5
victorfneves99 <victorfneves99@gmail.com>
2025-05-09 15:26:59 -0300

    feat: aprimorando swagger e ajuste nome tabela

--- src/main/java/com/pan/backend/adapters/out/persistence/ClienteEntity.java
+++ src/main/java/com/pan/backend/adapters/out/persistence/ClienteEntity.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name = "cliente")
 public class ClienteEntity {
     @Id
     private String cpf;
