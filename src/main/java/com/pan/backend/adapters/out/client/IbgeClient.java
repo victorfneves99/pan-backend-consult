9d5b862ce9b32b91e097d091b8b0ba63a4443bfb
victorfneves99 <victorfneves99@gmail.com>
2025-05-09 14:13:48 -0300

    feat: adicionando cache

--- src/main/java/com/pan/backend/adapters/out/client/IbgeClient.java
+++ src/main/java/com/pan/backend/adapters/out/client/IbgeClient.java
@@ -10,13 +10,17 @@ import org.springframework.web.client.RestTemplate;
 import com.pan.backend.adapters.out.client.dto.EstadoResponse;
 import com.pan.backend.adapters.out.client.dto.MunicipioResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class IbgeClient {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Cacheable("estados")
     public List<EstadoResponse> buscarEstados() {
+        log.info("Buscando estados");
         String url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
         EstadoResponse[] estados = restTemplate.getForObject(url, EstadoResponse[].class);
         return Arrays.asList(estados);
@@ -24,6 +28,7 @@ public class IbgeClient {
 
     @Cacheable(value = "municipios", key = "#estadoId")
     public List<MunicipioResponse> buscarMunicipiosPorEstado(Long idEstado) {
+        log.info("Buscando municipios do estado {}", idEstado);
         String url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/{id}/municipios";
         MunicipioResponse[] municipios = restTemplate.getForObject(url, MunicipioResponse[].class, idEstado);
         return Arrays.asList(municipios);
